6db87ea1dfc9efeaee5ca04261a19081bcfb4d8a
smealio <smealio@users.noreply.github.com>
2022-03-04 17:36:12 +0000

    deploy: 7f86d018a7b04c5d86d20e754e744cebee6c9850

---
+++ _next/static/K_OXeGY0yBkQxWJUnfC_U/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/8-c87e20040cc8dd4d.js","static/css/7fc448add8066f17.css","static/chunks/pages/index-23dfbfd847992e46.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- components/shared/utils.js
+++
@@ -1,3 +0,0 @@
-export const checkOnlyUnique = (value, index, self) => {
-  return self.indexOf(value) === index;
-}
