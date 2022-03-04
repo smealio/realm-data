17d47b6edae9a883311ad72438bc8780884c382c
Adam Smeal <adam@spectera.io>
2022-03-04 09:22:29 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
-  esling: {
+  eslint: {
     ignoreDuringBuilds: true,
   }
 }
