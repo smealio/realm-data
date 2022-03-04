6b133786e34e366ba9accc360ed2e16fbae0334c
Adam Smeal <adam@spectera.io>
2022-03-04 09:52:08 -0700

    here we go

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: '/realm-data/',
   eslint: {
     ignoreDuringBuilds: true,
   }
