bff0201c2d80660b2bda7005aef66bac0da4915c
Adam Smeal <adam@spectera.io>
2022-03-04 10:48:13 -0700

    eslint

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  eslint : {
+    ignoreDuringBuilds: true,
+  }
+
+  }
 }
 
 module.exports = nextConfig
