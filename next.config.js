968b61d17dbed170314209fc74351dee259d6d47
Adam Smeal <adam@spectera.io>
2022-03-04 10:17:55 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const withNextOptimizedImages = require('next-optimized-images');
 
-const nextConfig = withNextOptimizedImages([{
+const nextConfig = withNextOptimizedImages({
   reactStrictMode: true,
   assetPrefix: '/realm-data/',
   eslint: {
     ignoreDuringBuilds: true,
   }
-}
-])
+})
 
 module.exports = nextConfig
